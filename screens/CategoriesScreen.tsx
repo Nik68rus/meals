285ea70e96dd2a89ed35b484b88df6d2ce2b3349
Nik68rus <nik200x@mail.ru>
2023-03-01 18:06:03 +0300

    initial commit

---
+++ screens/CategoriesScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const CategoriesScreen = () => {
+  return (
+    <View>
+      <Text>Categories</Text>
+    </View>
+  );
+};
+
+export default CategoriesScreen;
---
+++ types/index.ts
@@ -0,0 +1,11 @@
+export enum Affordability {
+  AFFORDABLE = 'affordable',
+  PRICEY = 'pricey',
+  LUXURIOUS = 'luxurious',
+}
+
+export enum Complexity {
+  SIMPLE = 'simple',
+  CHALLENGING = 'challenging',
+  HARD = 'hard',
+}
